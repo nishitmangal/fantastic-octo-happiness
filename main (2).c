c9fe9d7e11471b6f18d8d41342c6c6b385d8dc15
nishitmangal <66747676+nishitmangal@users.noreply.github.com>
2021-10-01 23:09:00 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+  char calculations;
+  double firstnumber, secondnumber;
+  printf("Enter what you want to do (+, -, *, /): ");
+  scanf("%c", &calculations);
+  printf("Enter two numbers: ");
+  scanf("%lf %lf", &firstnumber, &secondnumber);
+
+  switch (calculations) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", firstnumber, secondnumber, firstnumber + secondnumber);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", firstnumber, secondnumber, firstnumber * secondnumber);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", firstnumber, secondnumber, firstnumber / secondnumber);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", firstnumber, secondnumber, firstnumber - secondnumber);
+      break;
+    default:
+      printf("Error! calculation sign is not correct");
+  }
+
+  return 0;
+}
